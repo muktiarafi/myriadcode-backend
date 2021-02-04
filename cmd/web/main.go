74c62a27f292f109ee285adc029adc11eed19fae
muktiarafi <muktiarafi@gmail.com>
2021-02-04 17:09:50 +0700

    configure main function and db env

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/muktiarafi/myriadcode-backend/internal/configs"
+	"github.com/muktiarafi/myriadcode-backend/internal/driver"
+	"github.com/muktiarafi/myriadcode-backend/internal/handler"
+	"github.com/muktiarafi/myriadcode-backend/internal/helpers"
 	"github.com/muktiarafi/myriadcode-backend/internal/logs"
+	"github.com/muktiarafi/myriadcode-backend/internal/repository"
 	"github.com/muktiarafi/myriadcode-backend/internal/router"
+	"github.com/muktiarafi/myriadcode-backend/internal/service"
+	"log"
 	"net/http"
 )
 
+const portNumber = ":8000"
 
 func main() {
 	app := configs.NewAppConfig()
+	app.Logger = logs.NewLogger()
 
-	logs.SetupLogs(app)
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	db, err := driver.ConnectSQL(
+		configs.PostgresDSN(),
+		app,
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer db.SQL.Close()
 
 	r := router.SetRouter()
 
-	http.ListenAndServe(":8000", r)
+	helpers.NewHelper(app)
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(&userRepository)
+	userHandler := handler.NewUserHandler(&userService)
+	userHandler.Route(r)
+
+	log.Println("connected to port 8000")
+	http.ListenAndServe(portNumber, r)
 }
