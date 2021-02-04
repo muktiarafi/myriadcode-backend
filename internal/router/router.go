bc4645ae91ee44c8b9d58d8c4f8892373573da95
muktiarafi <muktiarafi@gmail.com>
2021-02-04 13:16:10 +0700

    update router

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,16 +3,12 @@ package router
 import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/muktiarafi/myriadcode-backend/internal/controllers"
-	"github.com/muktiarafi/myriadcode-backend/internal/handler"
-	"net/http"
 )
 
-func SetRouter() http.Handler {
+func SetRouter() *chi.Mux {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.SetHeader("Content-Type", "application/json"))
-	mux.Method(http.MethodGet, "/", handler.CustomHandler(controllers.Test))
 
 	return mux
 }
