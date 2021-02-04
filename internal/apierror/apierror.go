9e27059c110f80ffb182103127b811690bc8d24f
muktiarafi <muktiarafi@gmail.com>
2021-02-04 13:11:53 +0700

    update apierror

--- internal/apierror/apierror.go
+++ internal/apierror/apierror.go
@@ -16,6 +16,19 @@ func (e *Error) Error() string {
 	return fmt.Sprintf("API Error: %s", e.Err.Error())
 }
 
+func checkWarning(warning *string, value string) {
+	if len(*warning) == 0 {
+		*warning = value
+	}
+}
+
 func NewInternalServerError(err error, warning string) *Error {
+	checkWarning(&warning, InternalServerError.Error())
 	return &Error{http.StatusInternalServerError, err, InternalServerError, warning}
+}
+
+func NewBadRequestError(err error, warning string) *Error {
+	kind := BadRequestError
+	checkWarning(&warning, kind.Error())
+	return &Error{http.StatusBadRequest, err, kind, warning}
 }
